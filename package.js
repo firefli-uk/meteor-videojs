9ddfb00561b89233558b0be4dd84fe8ea731e5eb
Matt Mills <github@firefli.co.uk>
2017-02-17 22:51:12 +0000

    Update package.js

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
-  name: 'jrudio:videojs',
+  name: 'firefli-uk:videojs',
   summary: 'VideoJS v5.10.4',
   version: '5.10.4',
-  git: 'https://github.com/jrudio/meteor-videojs.git'
+  git: 'https://github.com/firefli-uk/meteor-videojs.git'
 })
 
 Package.onUse(function(api) {
